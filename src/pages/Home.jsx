cc6cffa562e9056c58dc07b2d347e08735900204
devCesarRamos <cesarramos17@hotmail.com>
2023-11-21 23:29:22 +0000

    Update Hero, Sidebar, Footer & Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React, {useContext} from 'react';
 import { ProductContext } from '../contexts/ProductContext';
-import Product from '../components/Product'
+import Product from '../components/Product';
+import Hero from '../components/Hero';
 
 
 const Home = () => {
@@ -9,7 +10,9 @@ const Home = () => {
     return ( item.category === "men's clothing" || item.category === "women's clothing"
     );
   });
-  return <div>
+  return (
+  <div>
+    <Hero />
     <section className='py-16'>
       <div className="container mx-auto">
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-[30px] max-w-sm mx-auto md:max-w-none md:mx-0'>
@@ -19,7 +22,8 @@ const Home = () => {
         </div>
       </div>
     </section>
-  </div>;
+  </div>
+  )
 };
 
 export default Home;
